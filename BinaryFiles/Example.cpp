4ec6868774d7834311a6b5efdbf618478637999f
Timithy Snedden <s189066@students.aie.edu>
2018-10-12 16:03:50 -0700

    Changed the view and delete to view and delete after you close

--- BinaryFiles/Example.cpp
+++ BinaryFiles/Example.cpp
@@ -1,69 +1,69 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-
-bool testWrite(std::string filePath, std::string message)
-{
-	std::fstream file;
-
-	// open a file relative to the working directory of the project
-	file.open(filePath.c_str(), std::ios::out | std::ios::binary);
-
-	// verify that the file is open
-	if (file.fail())
-	{
-		std::cerr << "File not found." << std::endl;
-		return false;
-	}
-
-	// reset error flags (such as EOF)
-	file.clear();
-
-	// move the cursor to the end of the file for writing
-	file.seekp(0, std::ios_base::end);
-
-	// write a message to the text file
-	file << message;
-
-	file.flush();   // flush the output buffer, making sure all outputs are sent
-	file.close();   // close and release the file
-
-	return true;
-}
-
-std::string testRead(std::string filePath)
-{
-	std::fstream file;
-
-	// open a file relative to the working directory of the project
-	file.open(filePath.c_str(), std::ios::in | std::ios::binary);
-
-	// verify that the file is open
-	if (file.fail())
-	{
-		std::cerr << "File not found." << std::endl;
-		return "";
-	}
-
-	std::string buffer;
-	while (std::getline(file, buffer))  // iterates until error or EOF
-	{
-		// we don't really need to do anything while reading
-	}
-
-	// reset error flags (such as EOF)
-	file.clear();
-
-	// close and release the file
-	file.close();
-
-	return buffer;
-}
-
-int main()
-{
-	testWrite("test.bin", "yo");    // write "yo" to the file
-	std::cout << testRead("test.bin");  // read "yo" from the file
-
-	return 0;
-}
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//
+//bool testWrite(std::string filePath, std::string message)
+//{
+//	std::fstream file;
+//
+//	// open a file relative to the working directory of the project
+//	file.open(filePath.c_str(), std::ios::out | std::ios::binary);
+//
+//	// verify that the file is open
+//	if (file.fail())
+//	{
+//		std::cerr << "File not found." << std::endl;
+//		return false;
+//	}
+//
+//	// reset error flags (such as EOF)
+//	file.clear();
+//
+//	// move the cursor to the end of the file for writing
+//	file.seekp(0, std::ios_base::end);
+//
+//	// write a message to the text file
+//	file << message;
+//
+//	file.flush();   // flush the output buffer, making sure all outputs are sent
+//	file.close();   // close and release the file
+//
+//	return true;
+//}
+//
+//std::string testRead(std::string filePath)
+//{
+//	std::fstream file;
+//
+//	// open a file relative to the working directory of the project
+//	file.open(filePath.c_str(), std::ios::in | std::ios::binary);
+//
+//	// verify that the file is open
+//	if (file.fail())
+//	{
+//		std::cerr << "File not found." << std::endl;
+//		return "";
+//	}
+//
+//	std::string buffer;
+//	while (std::getline(file, buffer))  // iterates until error or EOF
+//	{
+//		// we don't really need to do anything while reading
+//	}
+//
+//	// reset error flags (such as EOF)
+//	file.clear();
+//
+//	// close and release the file
+//	file.close();
+//
+//	return buffer;
+//}
+//
+//int main()
+//{
+//	testWrite("test.bin", "yo");    // write "yo" to the file
+//	std::cout << testRead("test.bin");  // read "yo" from the file
+//
+//	return 0;
+//}
--- BinaryFiles/MonsterFolder.cpp
+++ BinaryFiles/MonsterFolder.cpp
@@ -3,7 +3,13 @@
 #include<string>
 using namespace std;
 int main() {
+	string Mfile1 = "MonstersData/1.bin";
+	string Mfile2 = "MonstersData/2.bin";
+	string Mfile3 = "MonstersData/3.bin";
 	fstream mFile;
+	fstream File1;
+	fstream File2;
+	fstream File3;
 	string MonsterID1;
 	string MonsterBio1;
 	string MonsterName1;
@@ -24,74 +30,65 @@ int main() {
 		cout << "a) Add a Monster" << endl;
 		cout << "b) Remove Monster by name" << endl;
 		cout << "c) View Monster Data" << endl;
-		cout << "d) Exit" << endl;
 		cin >> taskOne;
 		switch (taskOne) {
 		case'a':
 			cout << "You have selected: Add Monster" << endl;
 			cout << "Which number do you want to save the monster to" << endl;
-			cout<<"These are the save numebrs(0001,0002,0003)for the monster" << endl;
+			cout << "These are the Save number for Monster 1 or 2 or 3 Choose one of those numbers" << endl;
 			cin >> taskOneAdd;
 			switch (taskOneAdd)
 			{
 			case '1':
-				cout << "This is the save ID 0001.bin for the monster Please type that to confirm" << endl;
+				cout << "This is the save ID 1.bin for the monster Please type that to confirm " << endl;
 				cin >> MonsterID1;
 				cout << "Monster name" << endl;
 				cin >> MonsterName1;
 				cin.clear();
 				cin.ignore(10000, '\n');
 				cout << "Add bio please" << endl;
-				getline(cin,MonsterBio1);
+				getline(cin, MonsterBio1);
 				mFile.open("MonstersData/" + MonsterID1, ios::out, ios::binary);
-				mFile << "Monster Name: ";
 				mFile << MonsterName1;
 				mFile << "\n";
-				mFile << "Monster Bio: ";
 				mFile << MonsterBio1;
 				mFile << "\n";
 				mFile.flush();
 				mFile.close();
 				break;
 			case '2':
-				cout << "This is the save ID 0002.bin for the monster Please type that to confirm" << endl;
+				cout << "This is the save ID 2.bin for the monster Please type that to confirm" << endl;
 				cin >> MonsterID2;
 				cout << "Monster name" << endl;
 				cin >> MonsterName2;
 				cin.clear();
 				cin.ignore(10000, '\n');
 				cout << "Add bio please" << endl;
-				getline(cin,MonsterBio2);
+				getline(cin, MonsterBio2);
 				mFile.open("MonstersData/" + MonsterID2, ios::out, ios::binary);
-				mFile << "Monster Name: ";
 				mFile << MonsterName2;
 				mFile << "\n";
-				mFile << "Monster Bio: ";
 				mFile << MonsterBio2;
 				mFile << "\n";
 				mFile.flush();
 				mFile.close();
-				return true;
 				break;
 			case '3':
-				cout << "This is the save ID 0003.bin for the monster Please type that to confirm" << endl;
+				cout << "This is the save ID 3.bin for the monster Please type that to confirm" << endl;
 				cin >> MonsterID3;
 				cout << "Monster name" << endl;
 				cin >> MonsterName3;
 				cin.clear();
 				cin.ignore(10000, '\n');
 				cout << "Add bio please" << endl;
-				getline(cin,MonsterBio3);
+				getline(cin, MonsterBio3);
 				mFile.open("MonstersData/" + MonsterID3, ios::out, ios::binary);
-				mFile << "Monster Name: ";
 				mFile << MonsterName3;
 				mFile << "\n";
-				mFile << "Monster Bio: ";
 				mFile << MonsterBio3;
 				mFile << "\n";
 				mFile.flush();
 				mFile.close();
-				return true;
 				break;
 			default:
 				cout << "There is no *insert your number here* monster" << endl;
@@ -103,28 +100,28 @@ int main() {
 		case'b':
 			cout << "You have selected: Remove the monster" << endl;
 			cout << "Which monster do you want to delete" << endl;
-			cout << "1) 0001.bin" << endl;
-			cout << "2) 0002.bin" << endl;
-			cout << "3) 0003.bin" << endl;
+			cout << "1) 1.bin" << endl;
+			cout << "2) 2.bin" << endl;
+			cout << "3) 3.bin" << endl;
 			cout << "4)View the monster" << endl;
 			cin >> taskOneRemove;
 			switch (taskOneRemove)
 			{
 			case'1':
-				remove("MonstersData/0001.bin");
-				cout << "MonsterID: " << MonsterID1 << " is being deleted" << endl;
+				remove("MonstersData/1.bin");
+				cout << "MonsterID: " <<"1.bin" << " is being deleted" << endl;
 				system("pause");
 				return true;
 				break;
 			case '2':
-				remove("MonstersData/0002.bin");
-				cout << "MonsterID: " << MonsterID2 << " is being deleted" << endl;
+				remove("MonstersData/2.bin");
+				cout << "MonsterID: " << "2.bin" << " is being deleted" << endl;
 				system("pause");
 				return true;
 				break;
 			case '3':
-				remove("MonstersData/0003.bin");
-				cout << "MonsterID: " << MonsterID3 << " is being deleted" << endl;
+				remove("MonstersData/3.bin");
+				cout << "MonsterID: " << "3.bin" << " is being deleted" << endl;
 				system("pause");
 				return true;
 				break;
@@ -136,46 +133,71 @@ int main() {
 				return true;
 			}
 		case 'c':
+
 			cout << "You have selected: View Monster" << endl;
-			cout << "Which monster do you want to delete" << endl;
-			cout << "1) 0001.bin" << endl;
-			cout << "2) 0002.bin" << endl;
-			cout << "3) 0003.bin" << endl;
+			cout << "Which monster do you want to View" << endl;
+			cout << "1) 1.bin" << endl;
+			cout << "2) 2.bin" << endl;
+			cout << "3) 3.bin" << endl;
 			cin >> taskOneView;
 			switch (taskOneView) {
 			case'1':
+			{
+				string buffer1;
+				File1.open("MonstersData/1.bin", ios::in | ios::binary);
 				cout << "The monster Data" << endl;
-				cout << "Name: "<<MonsterName1 << endl;
-				cout << "Bio: " << MonsterBio1<< endl;
-				break;
-			case'2':
-				cout << "The monster Data" << endl;
-				cout << "Name: " << MonsterName2 << endl;
-				cout << "Bio: " << MonsterBio2 << endl;
-				break;
-			case'3':
-				cout << "The monster Data" << endl;
-				cout << "Name: " << MonsterName3 << endl;
-				cout << "Bio: " << MonsterName3<< endl;
+				getline(File1, buffer1);
+				cout << "Monster Name: " << buffer1 << endl;
+				getline(File1, buffer1);
+				cout << "Monster Bio: " << buffer1 << endl;
+				File1.flush();
+				File1.close();
 				break;
 			}
-		case'4':
-			cout << "There is no *insert your number here*for a monster" << endl;
-			system("pause");
-			return true;
-		
-	case'd':
-			cout << "good bye for now:-)" << endl;
-			system("pause");
-			return false;
-	case'e':
-			cout << "This is the secret letter good job" << endl;
-			system("pause");
-			return false;
-	default:
-			cout << "There is no *insert your number here* option for that I will close" << endl;
-			system("pause");
-			return false;
+			case'2':
+			{
+				string buffer2;
+				File2.open("MonstersData/2.bin", ios::in | ios::binary);
+				cout << "The monster Data" << endl;
+				getline(File2, buffer2);
+				cout << "Monster Name: " << buffer2 << endl;
+				getline(File2, buffer2);
+				cout << "Monster Bio: " << buffer2 << endl;
+				File2.flush();
+				File2.close();
+				break;
+			}
+			case'3':
+			{
+				string buffer3;
+				File3.open("MonstersData/3.bin", ios::in | ios::binary);
+				cout << "The monster Data" << endl;
+				getline(File3, buffer3);
+				cout << "Monster Name: " << buffer3 << endl;
+				getline(File3, buffer3);
+				cout << "Monster Bio: " << buffer3 << endl;
+				File3.flush();
+				File3.close();
+				break;
+			}
+			case'4':
+				cout << "There is no *insert your number here*for a monster" << endl;
+				system("pause");
+				return true;
+
+			case'd':
+				cout << "good bye for now:-)" << endl;
+				system("pause");
+				return false;
+			case'e':
+				cout << "This is the secret letter good job" << endl;
+				system("pause");
+				return false;
+			default:
+				cout << "There is no *insert your number here* option for that I will close" << endl;
+				system("pause");
+				return false;
+			}
 		}
 	}
 	system("pause");
